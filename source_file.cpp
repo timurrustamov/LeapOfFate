8fa05e8fd10db6ae2ea00f0a6fec945abb5b434d
rustam_t <rustam_t@git.epitech.eu>
2015-03-19 16:33:16 +0100

    +SDK

---
+++ source_file.cpp
@@ -0,0 +1,4 @@
+//
+// Created by rustam_t on 19/03/2015.
+//
+
---
+++ source_file.h
@@ -0,0 +1,8 @@
+//
+// Created by rustam_t on 19/03/2015.
+//
+
+#ifndef _LEAP_OF_FATE_SOURCE_FILE_H_
+#define _LEAP_OF_FATE_SOURCE_FILE_H_
+
+#endif //_LEAP_OF_FATE_SOURCE_FILE_H_
